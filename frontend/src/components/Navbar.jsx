a497cce00c28d45b816e11f6b2811bbf51d81967
apple <apple@apples-MacBook-Pro.local>
2025-05-31 17:56:14 +0530

    Navbar path updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,12 +4,12 @@ import "./Navbar.css"; // Create this CSS file for the animation
 
 const navItems = [
   { name: "Home", href: "/" },
-  { name: "About Georgia", href: "/about" },
+  { name: "About Georgia", href: "/AboutGeorgia" },
   { name: "How To Apply", href: "/HowToApply" },
-  { name: "Universities", href: "/universities" },
-  { name: "Student Testimonials", href: "/testimonials" },
-  { name: "FAQs", href: "/faqs" },
-  { name: "NMC Regulations", href: "/nmc-regulations" },
+  { name: "Universities", href: "/Universities" },
+  { name: "Student Testimonials", href: "/Testimonials" },
+  { name: "FAQs", href: "/FAQ" },
+  { name: "NMC Regulations", href: "/NMCReg" },
 ]; 
 
 const languages = [
